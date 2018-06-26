ca81af5eb67a16db4474f283f5b92d82b9169f53
w-shimaya <integral2d@gmail.com>
2018-06-27 01:26:30 +0900

    implemented extract_titles but it does not work

--- tvschedule.py
+++ tvschedule.py
@@ -5,5 +5,12 @@ import urllib2
 def getHTML(url):
     res = urllib2.urlopen(url)
     html = res.read()
+    f = open('log.html', 'w')
+    f.write(html)
+    f.close()
     return html
 
+def extract_titles(html):
+    matches = re.finditer(r'<a class=\"title', html)
+    for match in matches:
+        print(match.group())
